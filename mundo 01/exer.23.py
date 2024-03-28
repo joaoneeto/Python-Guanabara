9ec339398106d6cfc6978b552308dcce24c08d0c
João Rodrigues <joaorodriguesneeto@gmail.com>
2024-03-27 23:14:20 -0300

    update

--- mundo 01/exer.23.py
+++ mundo 01/exer.23.py
@@ -1,7 +1,12 @@
-num = str(input('Informe um numero: ')).strip()
+num = int(input('Informe um numero: '))
 print(f'Analisando o numero {num}...\n')
-num = num[::-1]
-print('Unidade: {}\n'
-      'Dezena: {}\n'
-      'Centena: {}\n'
-      'Milhar: {}'.format(num[0], num[1], num[2], num[3]))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Unidade: {u}\n'
+      f'Dezena: {d}\n'
+      f'Centena: {c}\n'
+      f'Milhar: {m}')
+
+# NÃO CONSEGUI REALIZAR EXERCICIO CORRETAMENTE
