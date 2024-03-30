4af8b9593b78ee1351d476c67896a6ddf321a84b
João Rodrigues <joaorodriguesneeto@gmail.com>
2024-03-30 20:46:19 -0300

    Update exer.28.py

--- mundo 01/exer.28.py
+++ mundo 01/exer.28.py
@@ -1,10 +1,13 @@
 import random
+import time
 numero = range(1,5)
 sortido = random.choice(numero)
 
 num = int(input('Digite um numero de 1 a 5: '))
+print('PROCESSANDO...')
+time.sleep(2)
 
 if num == sortido:
-    print('Você venceu! acertou o numero sortido, PARABENS')
+    print(f'Você venceu! acertou o numero sortido {sortido}, PARABENS')
 else:
-    print('Você errou, tente novamente!')
+    print(f'Você errou, eu pensei no numero {sortido}. Tente novamente ')
